fa3c73e572039dcc524c6783c8af04c41bf86183
Wahyu Hauzan Rafi <57913209+whauzan@users.noreply.github.com>
2022-07-01 13:40:19 +0700

    fix: maps api

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_config/flutter_config.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:hotelist_fe_mobile/screens/splash_screen.dart';
 
-void main() {
-    AndroidGoogleMapsFlutter.useAndroidViewSurface = true;
+void main() async {
+  AndroidGoogleMapsFlutter.useAndroidViewSurface = true;
+  WidgetsFlutterBinding.ensureInitialized(); // Required by FlutterConfig
+  await FlutterConfig.loadEnvVariables();
   runApp(const MyApp());
 }
 
